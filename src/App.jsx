ab39369b6733d50fbe0ceb9d58066845c380359a
Meet Jain <imeetjain07@outlook.com>
2024-09-21 13:38:39 +0530

    inititalizing again

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 const App = () => {
   return (
-    <>
-    App
+    <div id="main">
+      <div id="centrediv">
+        <h1>Welcome to React Web App</h1>
     <p>    
       {import.meta.env.VITE_API_KEY}
     </p>
-    </>
+    <button>Submit</button>
+    </div>
+    </div>
   )
 }
 
